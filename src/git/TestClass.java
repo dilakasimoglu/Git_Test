5c5b2181a8f1e03dda7a9594e3ad676255c6db86
dilakasimoglu <81911509+dilakasimoglu@users.noreply.github.com>
2021-04-04 17:14:53 +0200

    Update TestClass.java

--- src/git/TestClass.java
+++ src/git/TestClass.java
@@ -4,6 +4,10 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Git first commit");
+		System.out.println("Git seond commit");
+		System.out.println("Git third commit");
+		System.out.println("Git 4th commit");
 
 	}
 
